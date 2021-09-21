201ce7e3fdcb185fa3476b7cfb5d24f64cae604b
lost2003times <91131297+lost2003times@users.noreply.github.com>
2021-09-21 20:59:06 +0530

    Add files via upload

---
+++ MyfirstApp.cpp
@@ -0,0 +1,13 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+    cout << "My name is Jayman."<<endl;
+    cout << "Today is Monday";
+    cout << "Have a nice day.";
+    system("pause>0");
+}
